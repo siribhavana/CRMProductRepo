98b264e9c648d1ee848c07cfef4356721f9e9314
91765 <91765@Surekha-Laptop>
2025-12-26 11:33:07 +0530

    Initial commit

---
+++ com.comcast.vitigerGUIFramework/src/test/java/com/comcast/crm/producttest/ProductlinkTest.java
@@ -0,0 +1,28 @@
+package com.comcast.crm.producttest;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+
+public class ProductlinkTest {
+      
+	@Test
+	public void createNewProduct() 
+	{
+		WebDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("http://49.249.28.218:8888/");
+		driver.manage().window().maximize();
+		driver.findElement(By.name("user_name")).sendKeys("admin");
+		driver.findElement(By.name("user_password")).sendKeys("admin");
+		driver.findElement(By.id("submitButton")).click();
+		driver.findElement(By.linkText("Products")).click();
+		driver.findElement(By.xpath("//img[@alt='Create Product...']")).click();
+		driver.findElement(By.name("productname")).sendKeys("sam123");
+		driver.findElement(By.name("button")).click();
+	}
+}
